35a784fe1d5df2f031ec81e0058409d576cecbe9
Mallikarjun C V <mallikarjuncv12@gmail.com>
2025-10-25 13:22:28 +0530

    designed appointment pannel and connected it to the backend

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -4,8 +4,30 @@ export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
 
-    const value = {
+    const currency = '₹'
 
+    const calculateAge = (dob) => {
+        const today = new Date()
+        const birtDate = new Date(dob)
+
+        let age = today.getFullYear() - birtDate.getFullYear()
+        return age
+
+    }
+
+    const months = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    const slotDateFormat = (slot) => {
+
+        const dateArray = slot.split('_')
+        return dateArray[0] + " " + months[Number(dateArray[1])] + " " + dateArray[2]
+
+    }
+
+    const value = {
+        calculateAge,
+        slotDateFormat,
+        currency
     }
 
     return (
